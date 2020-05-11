3173e750aad33429a1ddf13d2e890e0592dbcac1
jackie <1539095742@qq.com>
2020-05-11 11:06:43 +0800

    完善功能

---
+++ serialportlibrary/src/main/jni/SerialPort.c
@@ -0,0 +1,85 @@
+//
+// Created by pengh on 2019/10/10.
+//
+
+#include "SerialPort.h"
+
+
+JNIEXPORT jobject JNICALL
+Java_com_jackiepenghe_serialportlibrary_SerialPort_open(JNIEnv *env, jclass clazz, jstring path,
+                                                        jint baudrate, jint flags) {
+    int fd;
+    speed_t speed;
+    jobject mFileDescriptor;
+
+    /* Check arguments */
+    {
+        speed = getBaudrate(baudrate);
+        if (speed == -1) {
+            LOGE("Invalid baudrate");
+            return NULL;
+        }
+    }
+
+    /* Opening device */
+    {
+        jboolean iscopy;
+        const char *path_utf = (*env)->GetStringUTFChars(env, path, &iscopy);
+        LOGD("Opening serial port %s with flags 0x%x", path_utf, O_RDWR | flags);
+        fd = open(path_utf, O_RDWR | flags);
+        LOGD("open() fd = %d", fd);
+        (*env)->ReleaseStringUTFChars(env, path, path_utf);
+        if (fd == -1) {
+            /* Throw an exception */
+            LOGE("Cannot open port");
+            return NULL;
+        }
+    }
+
+    /* Configure device */
+    {
+        struct termios cfg;
+        LOGD("Configuring serial port");
+        if (tcgetattr(fd, &cfg)) {
+            LOGE("tcgetattr() failed");
+            close(fd);
+            return NULL;
+        }
+
+        cfmakeraw(&cfg);
+        cfsetispeed(&cfg, speed);
+        cfsetospeed(&cfg, speed);
+
+        if (tcsetattr(fd, TCSANOW, &cfg)) {
+            LOGE("tcsetattr() failed");
+            close(fd);
+            return NULL;
+        }
+    }
+
+    /* Create a corresponding file descriptor */
+    {
+        jclass cFileDescriptor = (*env)->FindClass(env, "java/io/FileDescriptor");
+        jmethodID iFileDescriptor = (*env)->GetMethodID(env, cFileDescriptor, "<init>", "()V");
+        jfieldID descriptorID = (*env)->GetFieldID(env, cFileDescriptor, "descriptor", "I");
+        mFileDescriptor = (*env)->NewObject(env, cFileDescriptor, iFileDescriptor);
+        (*env)->SetIntField(env, mFileDescriptor, descriptorID, (jint) fd);
+    }
+
+    return mFileDescriptor;
+}
+
+JNIEXPORT void JNICALL
+Java_com_jackiepenghe_serialportlibrary_SerialPort_close(JNIEnv *env, jobject thiz) {
+    jclass SerialPortClass = (*env)->GetObjectClass(env, thiz);
+    jclass FileDescriptorClass = (*env)->FindClass(env, "java/io/FileDescriptor");
+
+    jfieldID mFdID = (*env)->GetFieldID(env, SerialPortClass, "mFd", "Ljava/io/FileDescriptor;");
+    jfieldID descriptorID = (*env)->GetFieldID(env, FileDescriptorClass, "descriptor", "I");
+
+    jobject mFd = (*env)->GetObjectField(env, thiz, mFdID);
+    jint descriptor = (*env)->GetIntField(env, mFd, descriptorID);
+
+    LOGD("close(fd = %d)", descriptor);
+    close(descriptor);
+}
